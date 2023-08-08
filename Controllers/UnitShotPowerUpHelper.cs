17b72d55b6a57c9885dc1996130c6e5d32079020
LukeZhuang <lukebloodytears@gmail.com>
2023-08-08 01:28:40 -0400

    refactor UnitShotPowerUpHelper

---
+++ Controllers/UnitShotPowerUpHelper.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using THLWToolBox.Data;
+using THLWToolBox.Models.DataTypes;
+using THLWToolBox.Models.ViewModels;
+using static THLWToolBox.Models.ViewModels.UnitShotPowerUpHelperViewModel;
+
+namespace THLWToolBox.Controllers
+{
+    public class UnitShotPowerUpHelper : Controller
+    {
+        private readonly THLWToolBoxContext _context;
+
+        public UnitShotPowerUpHelper(THLWToolBoxContext context)
+        {
+            _context = context;
+        }
+
+        // POST: UnitShotPowerUpHelper
+        public async Task<IActionResult> Index(UnitShotPowerUpHelperViewModel request)
+        {
+            if (_context.PictureData == null)
+            {
+                return Problem("Entity set 'THLWToolBoxContext.PlayerUnitData' is null.");
+            }
+
+            // --- query data tables ---
+            var unitTable = from pud in _context.PlayerUnitData select pud;
+            var unitList = await unitTable.Distinct().ToListAsync();
+
+            var shotTable = from pusd in _context.PlayerUnitShotData select pusd;
+            var shotList = await shotTable.Distinct().ToListAsync();
+
+            var spellcardTable = from puscd in _context.PlayerUnitSpellcardData select puscd;
+            var spellcardList = await spellcardTable.Distinct().ToListAsync();
+
+            Dictionary<int, PlayerUnitShotData> shotDict = new();
+            foreach (var shotRecord in shotList)
+                shotDict[shotRecord.id] = shotRecord;
+
+            Dictionary<int, PlayerUnitSpellcardData> spellcardDict = new();
+            foreach (var spellcardRecord in spellcardList)
+                spellcardDict[spellcardRecord.id] = spellcardRecord;
+            // ------ query end ------
+
+
+            List<PlayerUnitData> queryUnits = new();
+            List<UnitShotPowerUpHelperDisplayModel> powerUpDatas = new();
+
+            if (request.UnitSymbolName != null && request.UnitSymbolName.Length > 0)
+            {
+                foreach (var unitRecord in unitList)
+                {
+                    string curUnitSymbolName = unitRecord.name + unitRecord.symbol_name;
+                    if (!request.UnitSymbolName.Equals(curUnitSymbolName))
+                        continue;
+                    queryUnits.Add(unitRecord);
+                    List<UnitShotPowerUpHelperDisplayModel> unitPowerUpDatas = new()
+                    {
+                        new UnitShotPowerUpHelperDisplayModel(new AttackData("扩散", shotDict[unitRecord.shot1_id])),
+                        new UnitShotPowerUpHelperDisplayModel(new AttackData("集中", shotDict[unitRecord.shot2_id])),
+                        new UnitShotPowerUpHelperDisplayModel(new AttackData("一符", spellcardDict[unitRecord.spellcard1_id])),
+                        new UnitShotPowerUpHelperDisplayModel(new AttackData("二符", spellcardDict[unitRecord.spellcard2_id])),
+                        new UnitShotPowerUpHelperDisplayModel(new AttackData("终符", spellcardDict[unitRecord.spellcard5_id])),
+                    };
+                    powerUpDatas.AddRange(unitPowerUpDatas);
+                }
+            }
+
+            request.QueryUnits = queryUnits;
+            request.PowerUpDatas = powerUpDatas;
+
+            return View(request);
+        }
+    }
+}
---
+++ Models/ViewModels/UnitShotPowerUpHelperViewModel.cs
@@ -0,0 +1,31 @@
+﻿using THLWToolBox.Models.DataTypes;
+
+namespace THLWToolBox.Models.ViewModels
+{
+    public class UnitShotPowerUpHelperViewModel
+    {
+        // Display model definition
+        public class UnitShotPowerUpHelperDisplayModel
+        {
+            public AttackData attack_data { get; set; }
+            public UnitShotPowerUpHelperDisplayModel(AttackData attack_data)
+            {
+                this.attack_data = attack_data;
+            }
+        }
+
+        // Display models
+        public List<PlayerUnitData>? QueryUnits { get; set; }
+        public List<UnitShotPowerUpHelperDisplayModel>? PowerUpDatas { get; set; }
+
+        // Webpage query Parameters
+        public string? UnitSymbolName { get; set; }
+
+
+        // Methods
+        public static string CreatePowerUpRateStr(int powerUpRate)
+        {
+            return (0.01 * powerUpRate).ToString("0.00");
+        }
+    }
+}
