eee0ab99ebc193a2c5748e5d53be276254b6f6b3
LukeZhuang <lukebloodytears@gmail.com>
2023-08-15 18:52:21 -0400

    change numpy to native numsharp

--- Controllers/UnitShotSpiritRecycleHelper.cs
+++ Controllers/UnitShotSpiritRecycleHelper.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using NumSharp;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Numpy;
-using Python.Runtime;
 using THLWToolBox.Data;
 using THLWToolBox.Models.DataTypes;
 using THLWToolBox.Models.ViewModels;
@@ -28,7 +27,7 @@ namespace THLWToolBox.Controllers
         // POST: UnitShotSpiritRecycleHelper
         public async Task<IActionResult> Index(UnitShotSpiritRecycleHelperViewModel request)
         {
-            if (_context.PictureData == null)
+            if (_context.PlayerUnitData == null)
                 return Problem("Entity set 'THLWToolBoxContext.PlayerUnitData' is null.");
 
             // --- query data tables ---
@@ -106,39 +105,42 @@ namespace THLWToolBox.Controllers
             int enemyCount = (spiritRecycleInput.BulletRange == 1) ? 1 : request.EnemyCount.GetValueOrDefault(1);
             int hitRank = request.HitRank.GetValueOrDefault(0);
             if (hitRank < -10 || hitRank > 10)
-                throw new NotImplementedException();
+                throw new InvalidDataException();
             int sourceSmoke = request.SourceSmoke.GetValueOrDefault(0);
             int targetCharge = request.TargetCharge.GetValueOrDefault(0);
             int confidenceLevel = request.ConfidenceLevel.GetValueOrDefault(0);
 
-            using (Py.GIL())
+            var accumulatedSP = np.zeros(MONTE_CARLO).astype(typeof(double));
+            foreach (List<MagazineInfo> boost in spiritRecycleInput.BoostsInfo)
             {
-                var accumulatedSP = np.zeros(MONTE_CARLO);
-                foreach (List<MagazineInfo> boost in spiritRecycleInput.BoostsInfo)
+                foreach (MagazineInfo magazineInfo in boost)
                 {
-                    foreach (MagazineInfo magazineInfo in boost)
+                    int bulletCount = magazineInfo.BulletValue * enemyCount;
+                    bool isSureHit = magazineInfo.IsSureHit;
+                    double actualHit = GetActualHitRate(magazineInfo.Hit, hitRank, sourceSmoke, targetCharge);
+                    var aaa = spRate * np.random.randint(3, 8, new int[] { bulletCount, MONTE_CARLO }) * 0.04;
+                    var spiritRecycleMatrix = np.floor(spRate * np.random.randint(3, 8, new int[] { bulletCount, MONTE_CARLO }) * 0.04) * 0.01;
+                    if (!isSureHit)
                     {
-                        int bulletCount = magazineInfo.BulletValue * enemyCount;
-                        bool isSureHit = magazineInfo.IsSureHit;
-                        double actualHit = GetActualHitRate(magazineInfo.Hit, hitRank, sourceSmoke, targetCharge);
-                        var spiritRecycleMatrix = np.floor(spRate * np.random.randint(3, 8, new int[] { bulletCount, MONTE_CARLO }) * 0.04) * 0.01;
-                        if (!isSureHit)
-                        {
-                            var hitMask = np.random.rand(bulletCount, MONTE_CARLO) < actualHit;
-                            spiritRecycleMatrix *= hitMask;
-                        }
-                        var magazineSP = np.sum(spiritRecycleMatrix, 0);
-                        accumulatedSP += magazineSP;
+                        var hitMask = np.random.rand(bulletCount, MONTE_CARLO) < actualHit;
+                        spiritRecycleMatrix *= hitMask.astype(typeof(double));
                     }
-                    var tmpSP = np.sort(np.copy(accumulatedSP));
-                    double spiritRecycle;
-                    if (confidenceLevel == 0)
-                        spiritRecycle = np.average(tmpSP);
-                    else
-                        spiritRecycle = double.Parse(tmpSP[Convert.ToInt32(MONTE_CARLO * (1000 - confidenceLevel) / 1000.0)].repr);
-                    spiritRecycles.Add(spiritRecycle);
+                    spiritRecycleMatrix = spiritRecycleMatrix.astype(typeof(float));  // workaround for numsharp 0.30.0
+                    var magazineSP = np.sum(spiritRecycleMatrix, 0).astype(typeof(double));
+                    accumulatedSP += magazineSP;
                 }
+                double spiritRecycle;
+                if (confidenceLevel == 0)
+                    spiritRecycle = accumulatedSP.mean();
+                else
+                {
+                    var sortIndex = accumulatedSP.copy().argsort<double>();
+                    int confidenceIndex = sortIndex[Convert.ToInt32(MONTE_CARLO * (1000 - confidenceLevel) / 1000.0)];
+                    spiritRecycle = accumulatedSP[confidenceIndex];
+                }
+                spiritRecycles.Add(spiritRecycle);
             }
+
             return spiritRecycles;
         }
 
