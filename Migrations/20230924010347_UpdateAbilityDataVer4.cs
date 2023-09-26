6407fbe04fb89a4be0f5585f46b2f831035b79c1
LukeZhuang <lukebloodytears@gmail.com>
2023-09-26 00:53:27 -0400

    update PlayerUnitAbilityData according to v4.0 change

---
+++ Migrations/20230924010347_UpdateAbilityDataVer4.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace THLWToolBox.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAbilityDataVer4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "boost_buff_effect_type",
+                table: "PlayerUnitAbilityData",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "purge_buff_effect_type",
+                table: "PlayerUnitAbilityData",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "boost_buff_effect_type",
+                table: "PlayerUnitAbilityData");
+
+            migrationBuilder.DropColumn(
+                name: "purge_buff_effect_type",
+                table: "PlayerUnitAbilityData");
+        }
+    }
+}
