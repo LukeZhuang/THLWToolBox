06743723b3707c0829bbeb9c9d56a8c1be2de4c1
LukeZhuang <lukebloodytears@gmail.com>
2023-12-18 13:02:48 -0500

    12/18/2023 updated master package to v4032

--- Helpers/GeneralHelper.cs
+++ Helpers/GeneralHelper.cs
@@ -11,8 +11,8 @@ namespace THLWToolBox.Helpers
 {
     public class GeneralHelper
     {
-        public static string VERSION_STR = "v2.1";
-        public static string DATA_UPDATE_DATE = "2023/10/30";
+        public static string VERSION_STR = "v2.1.1";
+        public static string DATA_UPDATE_DATE = "2023/12/18";
 
         // For HTML
         public static string GetImageHtmlRaw(string imgUrl)
